0b25019d3ba8524650ac679a6cc3b66bf2a0b59a
Kirill <kirill.gagarski@gmail.com>
2023-01-05 22:26:49 +0300

    m

--- vxutil-vertigram/src/main/kotlin/ski/gagar/vxutil/vertigram/throttling/RateLimitExceededException.kt
+++ vxutil-vertigram/src/main/kotlin/ski/gagar/vxutil/vertigram/throttling/RateLimitExceededException.kt
@@ -1,7 +1,9 @@
 package ski.gagar.vxutil.vertigram.throttling
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import ski.gagar.vxutil.jackson.BadRequest
 import java.time.Duration
 
+@JsonIgnoreProperties("message", "suppressed", "localizedMessage")
 class RateLimitExceededException(msg: String? = null, cause: Throwable? = null, val retryIn: Duration?) :
     Exception(msg, cause), BadRequest
