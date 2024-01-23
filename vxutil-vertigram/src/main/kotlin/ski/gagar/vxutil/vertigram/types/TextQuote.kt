eba7725388aa46c54905e51cff3d692a3a6f9437
Kirill <kirill.gagarski@gmail.com>
2024-01-23 21:21:42 +0100

    Hotfix

--- vxutil-vertigram/src/main/kotlin/ski/gagar/vxutil/vertigram/types/TextQuote.kt
+++ vxutil-vertigram/src/main/kotlin/ski/gagar/vxutil/vertigram/types/TextQuote.kt
@@ -2,8 +2,8 @@ package ski.gagar.vxutil.vertigram.types
 
 data class TextQuote(
     val text: String,
-    val entities: List<MessageEntity>,
     val position: Int,
+    val entities: List<MessageEntity>? = null,
     @get:JvmName("getIsManual")
     val isManual: Boolean = false
 )
