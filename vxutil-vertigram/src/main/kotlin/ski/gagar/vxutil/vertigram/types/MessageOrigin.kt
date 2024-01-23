973b9c1327e40aa9deaf585a0da4eb133688d234
Kirill <kirill.gagarski@gmail.com>
2024-01-23 23:13:25 +0100

    MessageOriginChannel

--- vxutil-vertigram/src/main/kotlin/ski/gagar/vxutil/vertigram/types/MessageOrigin.kt
+++ vxutil-vertigram/src/main/kotlin/ski/gagar/vxutil/vertigram/types/MessageOrigin.kt
@@ -7,7 +7,8 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo
 @JsonSubTypes(
     JsonSubTypes.Type(value = MessageOriginUser::class, name = MessageOriginType.USER_STR),
     JsonSubTypes.Type(value = MessageOriginHiddenUser::class, name = MessageOriginType.HIDDEN_USER_STR),
-    JsonSubTypes.Type(value = MessageOriginChat::class, name = MessageOriginType.CHAT_STR)
+    JsonSubTypes.Type(value = MessageOriginChat::class, name = MessageOriginType.CHAT_STR),
+    JsonSubTypes.Type(value = MessageOriginChannel::class, name = MessageOriginType.CHANNEL_STR)
 )
 sealed interface MessageOrigin {
     val type: MessageOriginType
--- vxutil-vertigram/src/main/kotlin/ski/gagar/vxutil/vertigram/types/MessageOriginType.kt
+++ vxutil-vertigram/src/main/kotlin/ski/gagar/vxutil/vertigram/types/MessageOriginType.kt
@@ -8,11 +8,15 @@ enum class MessageOriginType {
     @JsonProperty(HIDDEN_USER_STR)
     HIDDEN_USER,
     @JsonProperty(CHAT_STR)
-    CHAT;
+    CHAT,
+    @JsonProperty(CHANNEL_STR)
+    CHANNEL,
+    ;
 
     companion object {
         const val USER_STR = "user"
         const val HIDDEN_USER_STR = "hidden_user"
         const val CHAT_STR = "chat"
+        const val CHANNEL_STR = "channel"
     }
 }
