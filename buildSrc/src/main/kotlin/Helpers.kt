b251a16c21051ee8524c98692c972f3f3691d1bf
Kirill <kirill.gagarski@gmail.com>
2025-03-16 17:46:00 +0100

    m

--- buildSrc/src/main/kotlin/Helpers.kt
+++ buildSrc/src/main/kotlin/Helpers.kt
@@ -2,10 +2,10 @@ import org.gradle.api.Project
 import java.time.LocalDate
 
 fun Project.years(): String {
-    val startYear = property("year.start").toString()
-    val currrentYear = LocalDate.now().year.toString()
-    return if (null == startYear || startYear == currrentYear)
+    val startYear = property("year.start")?.toString()
+    val currentYear = LocalDate.now().year.toString()
+    return if (null == startYear || startYear == currentYear)
             "${LocalDate.now().year}"
         else
-            "$startYear–$currrentYear"
+            "$startYear–$currentYear"
 }
